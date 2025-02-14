9683b928b23a30170008ed7c3acf004d9b2dee4c
Matheus Kipper <matheuskipper9@gmail.com>
2025-02-14 17:56:20 -0300

    adicionando validações nos input e utilizando uma biblioteca para formatar os inputs

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
 function calcular() {
-  let salario = parseFloat(document.getElementById("salario").value);
-  let percentual = parseFloat(document.getElementById("percentual").value);
+  let salario = parseFloat(
+    document
+      .getElementById("salario")
+      .value.replace(/\./g, "")
+      .replace(",", ".")
+  );
+  let percentual = parseFloat(
+    document
+      .getElementById("percentual")
+      .value.replace("%", "")
+      .replace(",", ".")
+  );
 
   if (isNaN(salario) || isNaN(percentual)) {
     alert("Os valores informados são inválidos!");
+    return;
+  } else if (salario === 0 || percentual === 0) {
+    alert("Os valores informados não podem ser zero!");
     return;
   }
 
   let aumento = (salario * percentual) / 100;
   let novosalario = salario + aumento;
 
-  document.getElementById("salario-atual").innerHTML = salario.toFixed(2);
-  document.getElementById("percentual-aumento").innerHTML =
-    percentual.toFixed(2);
-  document.getElementById("aumento-total").innerHTML = aumento.toFixed(2);
-  document.getElementById("novo-salario").innerHTML = novosalario.toFixed(2);
+  document.getElementById("salario-atual").innerHTML = salario
+    .toFixed(2)
+    .replace(".", ",");
+  document.getElementById("percentual-aumento").innerHTML = percentual
+    .toFixed(2)
+    .replace(".", ",");
+  document.getElementById("aumento-total").innerHTML = aumento
+    .toFixed(2)
+    .replace(".", ",");
+  document.getElementById("novo-salario").innerHTML = novosalario
+    .toFixed(2)
+    .replace(".", ",");
 
   document.getElementById("resultado-container").style.display = "block";
 }
@@ -32,6 +52,19 @@ function limpar() {
 
 function inicializar() {
   document.getElementById("resultado-container").style.display = "none";
+
+  new Cleave("#salario", {
+    numeral: true,
+    numeralThousandsGroupStyle: "thousand",
+    delimiter: ".",
+    numeralDecimalMark: ",",
+  });
+
+  new Cleave("#percentual", {
+    numeral: true,
+    numeralDecimalMark: ",",
+    suffix: "%",
+  });
 }
 
-window.onload = inicializar();
+window.onload = inicializar;
